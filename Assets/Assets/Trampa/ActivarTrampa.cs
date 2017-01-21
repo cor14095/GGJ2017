5632ca6362dd7e49f11bdb79c22aa38464444581
Joshua Franco <Joshua Franco>
2017-01-21 15:57:05 -0600

    agregar trampa y animacion

---
+++ Assets/Assets/Trampa/ActivarTrampa.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivarTrampa : MonoBehaviour {
+
+    Collider colision;
+    Animator animador;
+
+	// Use this for initialization
+	void Awake () {
+        animador = GetComponent<Animator>();
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        if (colision.tag == "Player"){
+            animador.SetBool("Trampa", true);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
