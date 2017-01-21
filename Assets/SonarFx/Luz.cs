58eb04911ede3928521f05b17d4128df8649072e
Joshua Franco <Joshua Franco>
2017-01-21 13:25:36 -0600

    agregar sonar

---
+++ Assets/SonarFx/Luz.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Luz : MonoBehaviour {
+    public Quaternion rotacion;
+    public float maximo = 0, minimo = 0;
+    public float tiempo;
+    Light luz;
+	// Use this for initialization
+	void Awake() {
+        transform.rotation = rotacion;
+        luz = GetComponent<Light>();
+        luz.range = maximo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (luz.range > minimo) {
+            luz.range -= Time.deltaTime * tiempo;
+        }
+	}
+}
