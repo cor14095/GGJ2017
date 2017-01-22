99dab78690f000da4744fc8ad2a483e008c541bc
adriano515 <valdezadriano@gmail.com>
2017-01-22 13:04:44 -0600

    no message

---
+++ Assets/Scripts/Movie.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+[RequireComponent(typeof(AudioSource))]
+
+public class Movie : MonoBehaviour {
+
+	public MovieTexture movie;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		audio = GetComponent<AudioSource> ();
+		audio.clip = movie.audioClip;
+		movie.Play ();
+		audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
